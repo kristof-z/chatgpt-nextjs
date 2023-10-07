0b0686390e169fb820f95764b518c405296b8647
kristof-z <kristofzorko@gmail.com>
2023-10-07 18:28:33 +0200

    added api key env file

--- src/pages/api/completion.js
+++ src/pages/api/completion.js
@@ -1,17 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
 export default async function completion(req, res) {
+  if (req.method === "POST") {
 
-  if (req.method === 'POST') {
-    const body = req.body
-    const prompt = body.prompt || ''
+    try {
+      const openai = new OpenAIApi(configuration);
+      const completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+          {"role": "system", "content": "You are a helpful assistant."},
+          {"role": "user", "content": "What are some famous astronomical observatories?"}
+        ]
+      )
 
-    const AiResponse = "React JS is a UI library for creating UIs..."
-
-    await new Promise((res) => setTimeout(res, 500))
-
-    return res.status(200).json({ result: AiResponse })
+      const aiResponse = (completion.data.choices[0].text).trim();
+      return res.status(200).json({result: aiResponse});
+    } catch(e) {
+      console.log(e.message);
+      return res.status(500).json({error: {message: e.message}});
+    }
   } else {
-    return res.status(500).json({ error: { message: "Invalid API Route" } })
+    return res.status(500).json({error: {message: "Invalid Api Route"}})
   }
 }
